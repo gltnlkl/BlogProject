e500ae1b984f4d0f24308b0c8585f228bdb71092
Gulten Ulukal <gulten.ulukal@gmail.com>
2022-03-09 17:43:48 +0300

    entity Post class has been added

---
+++ src/main/java/com/gulukal/blogspringtrestapi/entity/Post.java
@@ -0,0 +1,27 @@
+package com.gulukal.blogspringtrestapi.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name="posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}

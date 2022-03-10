2feabf10485e47c793beabebf1b0bc380823ff1c
Gulten Ulukal <gulten.ulukal@gmail.com>
2022-03-10 15:34:38 +0300

    pagination step 1

--- src/main/java/com/gulukal/blogspringtrestapi/controller/PostController.java
+++ src/main/java/com/gulukal/blogspringtrestapi/controller/PostController.java
@@ -33,7 +33,10 @@ public class PostController {
 
     //get all post rest api
     @GetMapping
-    public List<PostDto> getAllPosts() {
+    public List<PostDto> getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize
+    ) {
         return postService.getAllPosts();
     }
 

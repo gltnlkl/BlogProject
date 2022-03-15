418d19cd83e58224237bf41c6e7b78c309900380
Gulten Ulukal <gulten.ulukal@gmail.com>
2022-03-15 13:17:44 +0300

    validation annotations have been added in dto's

--- src/main/java/com/gulukal/blogspringtrestapi/dto/PostDto.java
+++ src/main/java/com/gulukal/blogspringtrestapi/dto/PostDto.java
@@ -2,6 +2,8 @@ package com.gulukal.blogspringtrestapi.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 /**
@@ -18,8 +20,22 @@ import java.util.Set;
 public class PostDto {
 
     private long id;
+
+    //validations for resource
+    //title should not be null or empty
+    //title should have at least 2 characters
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have 2 characters")
     private String title;
+
+    //description not be null or empty
+    //description should have at least 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have 10 characters")
     private String description;
+
+    //content not be null or empty
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 

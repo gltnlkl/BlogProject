e9854c056353a66a268ba4677bca600a4a8376e4
Gulten Ulukal <gulten.ulukal@gmail.com>
2022-03-15 14:54:21 +0300

    validation has been added for comment resource

--- src/main/java/com/gulukal/blogspringtrestapi/dto/CommentDto.java
+++ src/main/java/com/gulukal/blogspringtrestapi/dto/CommentDto.java
@@ -2,6 +2,10 @@ package com.gulukal.blogspringtrestapi.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 /**
  * @author Gulten Ulukal
  */
@@ -15,8 +19,24 @@ import lombok.*;
 public class CommentDto {
 
     private long id;
+
+    //validations for resource
+    //name should not be null or empty
+    //name should have at least 2 characters
+    @NotEmpty(message = "Name should not be null or empty")
+    @Size(min = 2, message = "Name should have min. 2 characters")
     private String name;
+
+    //email should not be null or empty
+    //email field validation
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+    //comment body should not be null or empty
+    //comment body must be min. 10 characters
+    @NotEmpty(message = "Commnet body should not be null or empty")
+    @Size(min = 10, message = "Commnet body should have min. 10 characters")
     private String body;
 
 

26adeec0eeced4651333b5009c06aa143cd41317
Gulten Ulukal <gulten.ulukal@gmail.com>
2022-03-17 12:15:06 +0300

    User entity has been created

---
+++ src/main/java/com/gulukal/blogspringtrestapi/entity/User.java
@@ -0,0 +1,24 @@
+package com.gulukal.blogspringtrestapi.entity;
+
+
+import lombok.Data;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Data
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"username"}),
+        @UniqueConstraint(columnNames = {"email"})
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+}
